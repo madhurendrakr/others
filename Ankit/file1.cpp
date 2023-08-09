25d1a714b9d1874208b18a92e74512220b6c4e71
madhurendrakr <88264792+madhurendrakr@users.noreply.github.com>
2023-08-09 23:42:20 +0530

    My second commit for other folder

---
+++ Ankit/file1.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "20UEC072";
+
+    return 0;
+}
---
+++ piyush/file1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    cout << "20UEC050" << endl;
+
+    return 0;
+}
