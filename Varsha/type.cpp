4fbe4565f44c3b908eaf18a1de9892fb42b2b39d
madhurendrakr <88264792+madhurendrakr@users.noreply.github.com>
2023-08-16 22:13:35 +0530

    Add files via upload

---
+++ Varsha/type.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std ;
+int main()
+{
+    int a,b;
+    cout<<"Enter a and b :";
+    cin>>a>>b;
+    int c=a+b;
+    cout<<c<<endl;
+    return 0 ;
+}
+// Due to the some of the other of the nation of the all of the sem and the some of the other of the nation of the all of the sem and the some of the other of the sem and the some of the other of the sem and the some of the other of the nation of the nation of the all of the sem and the other of the nation of the all of the sem and the some of the all of the sem and the some of the all of the sem and the osme of the all of the nation of the all of the fact and the soem of the all of the sem and hget some o fthe all of the fact and the some of the all of the sem and the some of the all of the nation of the all of the fact and the figure of the all of the sem and the sem and the some of the all of the nation of the all of the fact and the figure of the all of the sem and the some of the all of nation of the all of the sem due  of the all of the sem and the some of the all of the nation of theall of the and the some of the all of the fact of the all of the sem and the some of the all of the sem and the some of the all of the nation of the all of the  sem. And the some of the all of the fact and the some of the all of the sem and the some of the all of the nation of the all of the fact and the some of the all of the sem and the some of the all of the nation of the all of the nation of the all of the fact and the some of the all of the sem and the some of the all of the sem and the some of the all of the sem and the some of the all of the fact and the figure of the sem 
