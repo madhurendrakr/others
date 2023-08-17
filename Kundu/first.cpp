c8dbbb601c08422db0677ca36d26e873c4b51568
madhurendrakr <88264792+madhurendrakr@users.noreply.github.com>
2023-08-17 23:55:26 +0530

    Add files via upload

---
+++ Kundu/first.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std; 
+
+
+int main()
+{
+    Due to teh soem of the otther if the faact and the some of the other of the nation fot he other of the nation of the other nation and the some of the other of the fact and the some of the sem and the some of the other of the fact and the some of the other of the fact and the some of the other of the nation of the all of the fact and the some of the other of the nation and the
+   }
---
+++ Kundu/tut11.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include<vector>
+using namespace std; 
+int main(){
+    vector<int> v;
+    cout<<"size->"<<v.capacity()<<endl;
+    v.push_back(1);
+    cout<<"Size->"<<v.capacity()<<endl;
+
+
+// Due to the some of the other of the fact and the some of the other of the sem and the some of the other of the nation of the all of the fact and the figure of the all of the sem and the some of the other of the nation  of the all of the other of the nation of the othe rof the and the some of the all of the sem and the some of the all of the fact and the some of the other of the nationf ote 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+    
+}
