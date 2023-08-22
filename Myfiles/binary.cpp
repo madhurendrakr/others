9d07179cbe77905f25eff32270e7602ed5dbce0d
madhurendrakr <88264792+madhurendrakr@users.noreply.github.com>
2023-08-22 21:27:56 +0530

    Add files via upload

---
+++ Myfiles/binary.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// void binary(int num)
+// {
+//     // if (num == 0)
+//     // {
+//     //     int a=num;
+//     //     cout << a;
+//     //     return;
+//     // }
+//     // int rem = num % 2;
+//     // num = num / 2;
+//     // binary(num);
+//     // cout << rem;
+//     cout<<num;
+// }
+
+int main()
+{
+    int num = 5;
+    cout << num;
+    // binary(num);
+    return 0;
+}
---
+++ Myfiles/index.js
@@ -0,0 +1,11 @@
+
+
+let a ="Madhurendra Kumar 2";
+console.log(a);
+
+for(let j=0;j<=9;j++){
+    let b =7;
+    console.log(j + " " + b +"\n");
+}
+
+// console.log(b);
